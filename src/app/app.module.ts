29d90eb93efa660b4adef0616e810686366fc538
Jonathan Herrera <jonathanh1234@gmail.com>
2024-02-06 17:50:08 -0600

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ValentineButtonsComponent } from './valentine-buttons/valentine-buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValentineButtonsComponent
   ],
   imports: [
     BrowserModule,
